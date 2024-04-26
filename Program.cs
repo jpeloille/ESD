3f21228e2791e3a13cd0dbb035813c7a7ab5b57e
jpeloille <jpeloille@lagoon.nc>
2024-04-26 11:04:05 +1100

    Benclmark test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using System.Runtime.InteropServices;
+
 using ESD.Common;
 
 var regA = new SixteenBitsRegister();
@@ -25,3 +28,38 @@ Console.WriteLine(flags.Register.ToString("X2"));
 
 flags.C = true;
 Console.WriteLine(flags.Register.ToString("X2"));
+
+BenchmarkRunner.Run<Test>();
+
+[MemoryDiagnoser]
+public class Test()
+{
+    private byte A = 0xC0;
+    private byte M = 0x0;
+    private bool C = true;
+    private ushort result16;
+    private byte result8;
+    
+    [Benchmark]
+    public void NonBoxingAdc()
+    {            ushort leftOperand = 0x0000;
+        ushort rightOperand = 0x0000;
+
+        leftOperand |= A;
+        rightOperand |= M;
+            
+        result16 = leftOperand;
+        result16 += rightOperand;
+            
+        if (C) result16 += 1;
+        result8 = (byte)result16;
+    }
+
+    [Benchmark]
+    public void BoxingAdc()
+    {
+        result16 = (ushort)(A + M + (C ? 1 :0));
+        result8 = (byte)result16;
+        
+    }
+}
